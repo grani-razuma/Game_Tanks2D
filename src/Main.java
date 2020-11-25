28b30ca97d055706a984d91dfda255750279e09f
grani-razuma <arte.trofimov@gmail.com>
2020-11-25 17:38:19 +0300

    refactor commit 2

--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,92 @@ public class Main extends JPanel{
     final int BF_WIDTH = 576;
     final int BF_HEIGHT = 576;
 
+    final int OBJECT_SIZE = 64;
+
+    final int UP = 1;
+    final int DOWN = 2;
+    final int LEFT = 3;
+    final int RIGHT = 4;
+
+    String[][] objects =  {
+            {"B" ,"B" , "B" , "G" , "G" , "W" , "G" , "W" , "B"},
+            {"G" ,"G" , "G" , "G" , "G" , "G" , "G" , "W" , "B"},
+            {"B" ,"B" , "B" , "G" , "G" , "G" , "G" , "G" , "B"},
+            {"B" ,"B" , "B" , "G" , "G" , "G" , "G" , "B" , "B"},
+            {"G" ,"G" , "G" , "G" , "G" , "G" , "G" , "B" , "B"},
+            {"G" ,"G" , "G" , "G" , "G" , "G" , "G" , "G" , "B"},
+            {"B" ,"B" , "G" , "G" , "G" , "W" , "G" , "W" , "B"},
+            {"G" ,"B" , "B" , "G" , "G" , "W" , "W" , "W" , "B"},
+            {"B" ,"B" , "G" , "G" , "G" , "G" , "G" , "W" , "B"},
+    };
+
+
     // 1 - up, 2 - down, 3 - left, 4 - right.
-    int direction = 2;
+    int direction = 1;
 
     //координаты пули
-    int bulletX = 320;
-    int bulletY = 320;
+    int bulletX = -100;
+    int bulletY = -100;
 
     // Дефолтные координаты танка
-    int x = 256;
-    int y = 256;
+    int tankX = 256;
+    int tankY = 256;
 
     void moving(int direction) throws Exception {
         this.direction = direction;
 
         if(direction == 1){
-            y--;
+            tankY--;
         } else if (direction == 2){
-            y++;
+            tankY++;
         } else if (direction == 3){
-            x--;
+            tankX--;
         } else if (direction == 4) {
-            x++;
+            tankX++;
         }
         Thread.sleep(33);
         repaint();
     }
 
-    void runTheGame() throws Exception{
-        while (y!=0){
-            moving(1);
+    //Метод выстрела из танка
+    void fire() throws Exception{
+        bulletX = tankX + 25;
+        bulletY = tankY + 25;
+
+        //делаем условие, по которому летит пуля
+        while (bulletX > 0 && bulletX < BF_WIDTH &&
+                bulletY > 0 && bulletY < BF_HEIGHT){
+
+            switch (direction){
+                case 1:
+                    bulletY--;
+                    break;
+                case 2:
+                    bulletY++;
+                    break;
+                case 3:
+                    bulletX--;
+                    break;
+                case 4:
+                    bulletX++;
+                    break;
+            }
+            Thread.sleep(5);
+            repaint();
         }
+
+        //Когда пуля выходят цикла, она улетает за "карту"
+        bulletX = -100;
+        bulletY = -100;
+
     }
 
-        //логика движения по функциям
-        /*if(direction == 1){
-            moveUp();
-        } else if (direction == 2){
-            moveDown();
-        } else if (direction == 3){
-            moveLeft();
-        } else if (direction == 4){
-            moveRight();
-        }*/
+    void runTheGame() throws Exception{
+        //fire();
+        while(tankX != 0){
+            moving(LEFT);
+        }
+    }
 
 
     public static void main(String[] args) throws Exception {
@@ -70,24 +113,44 @@ public class Main extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        for (int y = 0; y < objects.length; y++) {
+            for (int x = 0; x < objects.length; x++) {
+                switch (objects[y][x]) {
+                    case "B":
+                        g.setColor(new Color(250, 132, 6));
+                        break;
+                    case "W":
+                        g.setColor(new Color(21, 175, 222));
+                        break;
+                    case "G":
+                        g.setColor(new Color(104, 87, 60));
+                        break;
+                }
+                g.fillRect(x * OBJECT_SIZE, y * OBJECT_SIZE, OBJECT_SIZE, OBJECT_SIZE);
+            }
+        }
+
+
         //Рисуем танк
         g.setColor(Color.YELLOW);
-        g.fillRect(x,y, 64,64);
+        g.fillRect(tankX, tankY,OBJECT_SIZE,OBJECT_SIZE);
 
         //Рисуем пушку
         g.setColor(Color.MAGENTA);
 
         //Добавляем положение пушки
         if (direction == 1) {
-            g.fillRect(x + 20,y - 12,24, 34);
+            g.fillRect(tankX + 20, tankY - 12,24, 34);
         }else if (direction == 2){
-            g.fillRect(x + 20,y + 42,24, 34);
+            g.fillRect(tankX + 20, tankY + 42,24, 34);
         }else if (direction == 3){
-            g.fillRect(x - 12,y + 20,34, 24);
+            g.fillRect(tankX - 12, tankY + 20,34, 24);
         }else if (direction == 4){
-            g.fillRect(x + 42 ,y + 20,34, 24);
+            g.fillRect(tankX + 42 , tankY + 20,34, 24);
         }
 
+        //Рисуем пулю
         g.setColor(Color.BLACK);
         g.fillRect(bulletX,bulletY, 14,14);
     }
